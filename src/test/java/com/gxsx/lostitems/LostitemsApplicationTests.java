115dbf1ce011a73dbd53975a9e65fcfe802e4f33
leejunsung <jsl4411@naver.com>
2020-12-19 20:06:45 +0900

    Initial commit

---
+++ src/test/java/com/gxsx/lostitems/LostitemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxsx.lostitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostitemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
